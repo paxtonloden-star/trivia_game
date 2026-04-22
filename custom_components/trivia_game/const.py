3b30a083243a48239700f32da850d44c4b34c1bc
paxtonloden-star <paxtonloden@gmail.com>
2026-04-22 02:16:01 -0500

    Add trivia_game constants

---
+++ custom_components/trivia_game/const.py
@@ -0,0 +1,18 @@
+DOMAIN = "trivia_game"
+STORAGE_VERSION = 1
+STORAGE_KEY = "trivia_game.storage"
+
+CONF_REMOTE_BASE_URL = "remote_base_url"
+CONF_DEFAULT_PACK_SOURCE = "default_pack_source"
+CONF_DEFAULT_ANSWER_SECONDS = "default_answer_seconds"
+CONF_DEFAULT_REVEAL_SECONDS = "default_reveal_seconds"
+CONF_DEFAULT_AUTO_NEXT = "default_auto_next"
+
+DEFAULT_TITLE = "Trivia Game"
+DEFAULT_REMOTE_BASE_URL = ""
+DEFAULT_PACK_SOURCE = "offline_curated"
+DEFAULT_ANSWER_SECONDS = 20
+DEFAULT_REVEAL_SECONDS = 5
+DEFAULT_AUTO_NEXT = True
+
+WS_EVENT_STATE = "state"
